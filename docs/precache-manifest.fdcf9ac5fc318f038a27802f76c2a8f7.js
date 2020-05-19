31cc0874d5c2c6060a4c7a8cef8f1b891eac6b26
Adrien Dion <adion81@gmail.com>
2020-05-19 15:59:20 -0500

    api fix

---
+++ docs/precache-manifest.fdcf9ac5fc318f038a27802f76c2a8f7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5815d88e45963ebcb3ed712aaa4a1465",
+    "url": "/devdion/index.html"
+  },
+  {
+    "revision": "ee9b772bc6a2eca2eb44",
+    "url": "/devdion/static/css/main.dffb4d91.chunk.css"
+  },
+  {
+    "revision": "320a4c75b3f2e44db44f",
+    "url": "/devdion/static/js/2.e62e48ed.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/devdion/static/js/2.e62e48ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9b772bc6a2eca2eb44",
+    "url": "/devdion/static/js/main.02fbf765.chunk.js"
+  },
+  {
+    "revision": "0a09e0efecff0cd06427",
+    "url": "/devdion/static/js/runtime-main.c0996f68.js"
+  },
+  {
+    "revision": "687eb5720968f9d502210b8a28797346",
+    "url": "/devdion/static/media/adrienProfile.687eb572.png"
+  },
+  {
+    "revision": "eeafa106286bed4cfdfb1001dc01a271",
+    "url": "/devdion/static/media/black-numbers-background_b4eu7wk3_thumbnail-full01.eeafa106.png"
+  },
+  {
+    "revision": "7f30204cf3119b544a414d0644f53216",
+    "url": "/devdion/static/media/csharp.7f30204c.png"
+  },
+  {
+    "revision": "1d4458264248256f2eadab3c56aabd30",
+    "url": "/devdion/static/media/frontEnd.1d445826.png"
+  },
+  {
+    "revision": "18f18ebc10b530c873518f49b46874c2",
+    "url": "/devdion/static/media/mean.18f18ebc.png"
+  },
+  {
+    "revision": "0527e51cc8fbdf853418f412a26e0758",
+    "url": "/devdion/static/media/menu-hex.0527e51c.svg"
+  },
+  {
+    "revision": "8e4c4b006fdf8fc97b2383afd3fee9b6",
+    "url": "/devdion/static/media/mern.8e4c4b00.png"
+  },
+  {
+    "revision": "135a21b0145ad62392516a6e0451cdec",
+    "url": "/devdion/static/media/python.135a21b0.png"
+  }
+]);
